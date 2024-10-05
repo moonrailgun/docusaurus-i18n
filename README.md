c058caae1b043a0d770d7ae96a800d919b71fdd7
moonrailgun <moonrailgun@gmail.com>
2024-10-05 16:44:46 +0800

    feat: add readme and move translateDocs action to function

---
+++ README.md
@@ -0,0 +1,28 @@
+## How to use
+
+Make sure you have the following in your docusaurus config:
+
+```ts
+i18n: {
+  defaultLocale: 'en',
+  locales: ['en', 'fr'],
+},
+```
+
+Then:
+
+Run this command:
+
+```bash
+OPENAI_API_KEY=sk-xxxxxx npx docusaurus-i18n
+```
+
+## Get Help
+
+```bash
+npx docusaurus-i18n --help
+```
+
+## Reference locale code
+
+[https://github.com/facebook/docusaurus/tree/main/packages/docusaurus-theme-translations/locales](https://github.com/facebook/docusaurus/tree/main/packages/docusaurus-theme-translations/locales)
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ import fs from 'fs-extra';
 import md5 from 'md5';
 import { translate } from './translate';
 
+const siteDir = options.project;
+
 async function main() {
-  const siteDir = options.project;
   const config = await loadSiteConfig(siteDir);
   if (!config.i18n) {
     throw new Error(
@@ -19,73 +20,86 @@ async function main() {
   const { defaultLocale, locales } = config.i18n;
   const targetLocales = locales.filter((l) => l !== defaultLocale);
 
+  if (targetLocales.length === 0) {
+    throw new Error(
+      'Please make sure you have more than one locale exclude default locale'
+    );
+  }
+
   for (const locale of targetLocales) {
     console.group(`Translating ${locale}:`);
 
-    // translate docs
-    const sourceDir = path.resolve(siteDir, './docs');
-    const targetDir = path.resolve(
-      siteDir,
-      './i18n',
-      locale,
-      './docusaurus-plugin-content-docs/current'
-    );
-
-    const documents = await fg(path.resolve(sourceDir, './**/*.md'));
-
-    for (const sourcePath of documents) {
-      const targetFile = path.relative(sourceDir, sourcePath);
-      const targetPath = path.resolve(targetDir, targetFile);
-
-      const { data, content } = matter.read(sourcePath);
-      const filehash = md5(content);
-
-      if (await fs.exists(targetPath)) {
-        // translated file existed and not changed, skip
-        const { data: targetData } = matter.read(sourcePath);
-
-        if (!targetData._i18n_hash) {
-          console.log('This file is created manual, skip:', targetFile);
-          continue;
-        }
-
-        if (targetData._i18n_hash === filehash) {
-          console.log('This source file not changed, skip:', targetFile);
-          continue;
-        }
-      }
-
-      console.log('Translating....', targetFile);
-
-      const { usage, content: translatedContent } = await translate(
-        content,
-        locale
-      );
-
-      await fs.mkdirp(path.dirname(targetPath));
-      await fs.writeFile(
-        targetPath,
-        matter.stringify(
-          {
-            content: translatedContent,
-          },
-          {
-            ...data,
-            _i18n_hash: filehash,
-          }
-        ),
-        'utf8'
-      );
-      console.log(
-        'Writed translated file into',
-        targetPath,
-        ', token usage:',
-        usage
-      );
-    }
+    await translateDocs(locale);
 
     console.groupEnd();
   }
 }
 
 main();
+
+/**
+ * translate docs
+ * @param locale
+ */
+async function translateDocs(locale: string) {
+  const sourceDir = path.resolve(siteDir, './docs');
+  const targetDir = path.resolve(
+    siteDir,
+    './i18n',
+    locale,
+    './docusaurus-plugin-content-docs/current'
+  );
+
+  const documents = await fg(path.resolve(sourceDir, './**/*.md'));
+
+  for (const sourcePath of documents) {
+    const targetFile = path.relative(sourceDir, sourcePath);
+    const targetPath = path.resolve(targetDir, targetFile);
+
+    const { data, content } = matter.read(sourcePath);
+    const filehash = md5(content);
+
+    if (await fs.exists(targetPath)) {
+      // translated file existed and not changed, skip
+      const { data: targetData } = matter.read(sourcePath);
+
+      if (!targetData._i18n_hash) {
+        console.log('This file is created manual, skip:', targetFile);
+        continue;
+      }
+
+      if (targetData._i18n_hash === filehash) {
+        console.log('This source file not changed, skip:', targetFile);
+        continue;
+      }
+    }
+
+    console.log('Translating....', targetFile);
+
+    const { usage, content: translatedContent } = await translate(
+      content,
+      locale
+    );
+
+    await fs.mkdirp(path.dirname(targetPath));
+    await fs.writeFile(
+      targetPath,
+      matter.stringify(
+        {
+          content: translatedContent,
+        },
+        {
+          ...data,
+          _i18n_hash: filehash,
+        }
+      ),
+      'utf8'
+    );
+    console.log(
+      'Writed translated file into',
+      targetPath,
+      ', token usage:',
+      usage
+    );
+  }
+}
